24135079e1162731e1df452bfdd45b9db8778ad3
aissaP5 <pitche2005@outlook.fr>
2026-05-25 16:10:11 +0100

    remove testimonials section completely

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import { About } from './sections/About';
 import { Skills } from './sections/Skills';
 import { Projects } from './sections/Projects';
 import { Services } from './sections/Services';
-import { Testimonials } from './sections/Testimonials';
 import { Contact } from './sections/Contact';
 
 function App() {
@@ -40,7 +39,6 @@ function App() {
         <Skills />
         <Projects />
         <Services />
-        <Testimonials />
         <Contact />
       </main>
     </div>
